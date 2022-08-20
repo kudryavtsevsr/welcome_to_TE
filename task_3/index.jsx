e076f93dfffd6fe1ceeca5ac01c9f60bd68bd33b
Sergey Kudryavtsev <kusa@bork.ru>
2022-08-20 18:45:30 +0300

    complete third task

--- task_3/index.jsx
+++ task_3/index.jsx
@@ -1,11 +1,14 @@
-import { Fragment, memo } from 'react';
+import { Fragment, memo, useMemo } from 'react';
 
 const MainComponent = ({
     user = { name: 'unknown', age: null } // default value for `props.user`
 }) => {
+
+    const userForChild = useMemo(() => user, [user.name, user.age]);
+
     return (
         <Fragment>
-            <ChildComponent user={user} />
+            <ChildComponent user={userForChild} />
         </Fragment>
     );
 };
